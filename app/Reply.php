65cbaa04583bb29f5314bc8322aa877c1d93d902
Bakay <gamempvs@gmail.com>
2019-09-15 22:09:05 +0600

    Added Laravel IDE Helper Generator

--- app/Reply.php
+++ app/Reply.php
@@ -2,8 +2,31 @@
 
 namespace App;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Reply
+ *
+ * @property int $id
+ * @property int $thread_id
+ * @property int $user_id
+ * @property string $body
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Reply newModelQuery()
+ * @method static Builder|Reply newQuery()
+ * @method static Builder|Reply query()
+ * @method static Builder|Reply whereBody($value)
+ * @method static Builder|Reply whereCreatedAt($value)
+ * @method static Builder|Reply whereId($value)
+ * @method static Builder|Reply whereThreadId($value)
+ * @method static Builder|Reply whereUpdatedAt($value)
+ * @method static Builder|Reply whereUserId($value)
+ * @mixin Eloquent
+ */
 class Reply extends Model
 {
     //
--- app/Thread.php
+++ app/Thread.php
@@ -2,8 +2,31 @@
 
 namespace App;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Thread
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $body
+ * @property string $title
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Thread newModelQuery()
+ * @method static Builder|Thread newQuery()
+ * @method static Builder|Thread query()
+ * @method static Builder|Thread whereBody($value)
+ * @method static Builder|Thread whereCreatedAt($value)
+ * @method static Builder|Thread whereId($value)
+ * @method static Builder|Thread whereTitle($value)
+ * @method static Builder|Thread whereUpdatedAt($value)
+ * @method static Builder|Thread whereUserId($value)
+ * @mixin Eloquent
+ */
 class Thread extends Model
 {
     public function path()
